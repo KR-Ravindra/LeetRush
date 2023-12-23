9f3553a242d84ffa9149dc0898777e113bfff865
Raja Ravindra Kathi <rajaravindra919@gmail.com>
2023-12-23 13:26:19 -0800

    Time: 166 ms (98.62%) | Memory: 24.6 MB (31.21%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,22 +1,45 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        preproduct = [1] * len(nums)
-        postproduct = [1] * len(nums)
+# solution:
+# -> here space complexity is O(n); result array doesnt count.
+# -> we compute prefix and postfix prodcut arrays; prefix array has at every index product of numbers from main nums array till index (not including index); similarly postfix has product till before that number from end of nums.
+# -> for prefix we traverse nums from 0 to end; postfix we do it end to 0
+# -> at last, for result array we multiply prefix[i]*postfix[i]
+        # preproduct = [1] * len(nums)
+        # postproduct = [1] * len(nums)
+        # result = [1] * len(nums)
+        # for index in range(len(nums)):
+        #     if index == 0:
+        #         preproduct[index] = 1
+        #         continue
+        #     preproduct[index] = preproduct[index - 1] * nums[index-1]
+        # for index in range(len(nums)-1, -1, -1):
+        #     if index == len(nums)-1:
+        #         postproduct[index] = 1
+        #         continue
+        #     postproduct[index] = postproduct[index + 1] * nums[index+1]
+        # for index in range(len(nums)):
+        #     result[index] = preproduct[index]*postproduct[index]
+        # return result
+# with space complexity as O(1):
+# -> instead of building two post and prefix arrays let us use variables and keep track of product and directly manipulate final array
+# -> traversing remains same; during prefix sequence we go from 0 to end and at each index we multiply prefix variable to current num, feed value to result, update prefix to current result; similarly for postfix sequence we do vice versa
+
+        prefix = 1
+        postfix = 1
         result = [1] * len(nums)
         for index in range(len(nums)):
             if index == 0:
-                preproduct[index] = 1
+                result[index] = prefix
                 continue
-            preproduct[index] = preproduct[index - 1] * nums[index-1]
-        print(preproduct)
+            result[index] = prefix * nums[index-1]
+            prefix = result[index]
         for index in range(len(nums)-1, -1, -1):
             if index == len(nums)-1:
-                postproduct[index] = 1
+                postfix = nums[index]
                 continue
-            postproduct[index] = postproduct[index + 1] * nums[index+1]
-        print(postproduct)
-        for index in range(len(nums)):
-            result[index] = preproduct[index]*postproduct[index]
+            result[index] = result[index] * postfix
+            postfix = postfix * nums[index]
         return result
 
                 
