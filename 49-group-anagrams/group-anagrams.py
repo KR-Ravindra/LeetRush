b02ffd93d48cce0465fd73d1e7b751c9b635fc6b
Raja Ravindra Kathi <rajaravindra919@gmail.com>
2023-12-22 21:04:12 -0800

    Time: 87 ms (94.79%) | Memory: 23.2 MB (6.68%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -2,13 +2,15 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         final_output = defaultdict(list)
         for str in strs:
-            # count = [0] * 26
+            #using algorithm learnt ascii
+            count = [0] * 26
 
-            # for char in str:
-            #     count[ord(char)-ord('a')] += 1
+            for char in str:
+                count[ord(char)-ord('a')] += 1
             
-            # final_output[tuple(count)].append(str)
-            final_output[tuple(sorted(str))].append(str)
+            final_output[tuple(count)].append(str)
+            #using sorting
+            # final_output[tuple(sorted(str))].append(str)
         
         return final_output.values()
 
