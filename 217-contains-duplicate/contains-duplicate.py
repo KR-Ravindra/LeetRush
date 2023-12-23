f0e5bf9a26d887a7c8a11d5e82a288b18874d70d
Raja Ravindra Kathi <rajaravindra919@gmail.com>
2023-12-23 12:05:45 -0800

    Time: 421 ms (98.89%) | Memory: 33 MB (24.30%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,17 +1,11 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        # by using hashset
-        hashset = set()
+# solution:
+# -> for each element we visit, let us store it in a hashset (why not list? in operation in list is O(1); in set is O(1))
+# -> for every element we see check in the hashset, if exists return True
+        seenHashSet = set()
         for num in nums:
-            if num in hashset:
+            if num in seenHashSet:
                 return True
-            else:
-                hashset.add(num)
-        # by using list
-        # seen = []
-        # for num in nums:
-        #     if num in seen:
-        #         return True
-        #     else:
-        #         seen.append(num)
-        # return False
+            seenHashSet.add(num)
+        return False
