29c18f4e0d4c8626d85898953540bc001a96d197
Raja Ravindra Kathi <rajaravindra919@gmail.com>
2023-12-23 11:43:22 -0800

    Time: 93 ms (82.28%) | Memory: 30.7 MB (6.05%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,5 +1,9 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+# solution:
+# -> First we will calculate the count of each character/number
+# -> then we plot it on frequency array, here frequency array could only be as big as len(nums)
+# -> we reverse iterate through our frequency array ([6: {1,2}, 5: {2,3}....]) and fill result list with the variable we have in set till k is len(result)
         count = defaultdict(int)
         for num in nums:
             count[num] += 1
