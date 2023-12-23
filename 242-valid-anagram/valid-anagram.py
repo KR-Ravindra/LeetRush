aaa92a0188f8e93270ed9c1f60214afa73c998bd
Raja Ravindra Kathi <rajaravindra919@gmail.com>
2023-12-22 21:12:59 -0800

    Time: 34 ms (99.41%) | Memory: 17.7 MB (5.64%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -4,7 +4,16 @@ class Solution:
         if len(s) != len(t):
             return False
         # by sorting
-        return sorted(s) == sorted(t)
- 
+        # return sorted(s) == sorted(t)
+        # by algorithm
+        countS, countT = defaultdict(int), defaultdict(int)
+        for index in range(len(s)):
+            countS[s[index]] = 1 + countS.get(s[index], 0)
+            countT[t[index]] = 1 + countT.get(t[index], 0)
+
+        for key in countS:
+            if countT[key] != countS[key]:
+                return False
+        return True
 
         
