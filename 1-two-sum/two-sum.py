08af9ca81b0eefa1fe5601cfef851e15b56489c9
Raja Ravindra Kathi <rajaravindra919@gmail.com>
2023-12-23 11:56:40 -0800

    Time: 64 ms (69.35%) | Memory: 18.6 MB (5.23%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,8 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen = {}
-        for index, eachnum in enumerate(nums):
-            diff = target-eachnum
-            if diff in seen:
-                return [index, seen[diff]]
-            seen[eachnum] = index
+# Solution:
+# -> for each number let us subtract it from target and see if the difference exists in list (but this `in` is a big problem O(n))
+# -> let us maintain HashMap and keep storing the numbers we see into it, and check if the diff is in the hashmap instead of whole list which is O(1)
+# -> This way we will immediately know the solution in O(n)
+        seenHashMap = {}
+        for index, num in enumerate(nums):
+            if target-num in seenHashMap:
+                return [index, seenHashMap[target-num]]
+            seenHashMap[num] = index
+        return False
