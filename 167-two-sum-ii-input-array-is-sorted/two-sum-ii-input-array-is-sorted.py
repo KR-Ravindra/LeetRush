06614ca1587552d30c04f7ded029dd9d5d821d61
Raja Ravindra Kathi <rajaravindra919@gmail.com>
2023-12-25 13:20:33 -0800

    Time: 106 ms (95.84%) | Memory: 18.2 MB (6.81%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,9 +1,26 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        seen = {}
-        for index, num in enumerate(numbers):
-            if target-num in seen:
-                print(f"{seen=} {target-num=} {num=}")
-                return [ seen[target-num]+1,index+1]
-            seen[num] = index    
-        return 
+# solution with memory not constant
+        # seen = {}
+        # for index, num in enumerate(numbers):
+        #     if target-num in seen:
+        #         print(f"{seen=} {target-num=} {num=}")
+        #         return [ seen[target-num]+1,index+1]
+        #     seen[num] = index    
+        # return 
+# solution with memory constant
+# -> using two pointers; since array is started, we will increase left if left+right < target; if left+right > target then we will decrease right.. as left to right values only increase.
+        left, right = 0, len(numbers)-1
+        while left < right:
+            if numbers[left] + numbers[right] == target:
+                return [left+1, right+1]
+            if numbers[left] + numbers[right] > target:
+                right -= 1
+                continue
+            if numbers[left] + numbers[right] < target:
+                left += 1
+                continue
+        return False
+    
+
+            
