f572c59d4126e27363cb18f7262037ba20d12bda
Raja Ravindra Kathi <rajaravindra919@gmail.com>
2023-12-25 13:53:57 -0800

    Time: 989 ms (72.76%) | Memory: 21.6 MB (11.57%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -17,6 +17,9 @@ class Solution:
                     left += 1
                     continue
                 left += 1
+                right -= 1
                 while nums[left] == nums[left-1] and left < right:
                     left += 1
+                while nums[right] == nums[right+1] and left < right:
+                    right -= 1
         return triplets
