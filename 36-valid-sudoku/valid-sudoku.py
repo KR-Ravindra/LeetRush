cc998ce37379f57f4bb03fef068448726a747896
Raja Ravindra Kathi <rajaravindra919@gmail.com>
2023-12-25 02:09:24 -0800

    Time: 99 ms (69.44%) | Memory: 17.3 MB (6.45%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,18 +1,47 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        columns = defaultdict(set)
-        rows = defaultdict(set)
-        submatrix = defaultdict(set)
+# solution:
+# -> so for each row, column and submatrix we need to check if the set has any duplicate numbers. Let us ignore the `.` in input and then include all the numbers in each row and column.
+# -> how do we consider submatrices? - two ways, one predefine them (only works for this). second is every location(x,y) in 9*9 submatrix on integer division with 3, will always belong to one of 3*3 sub matrix - eg (4,4) in 9,9 => (1,1)
+# way1:
+        # rowscount = defaultdict(set)
+        # columncount = defaultdict(set)
+        # submatrixcount = defaultdict(set)
 
-        for row in range(len(board)):
-            for column in range(len(board)):
-                if board[row][column] == '.':
+        # for i in range(len(board)):
+        #     for j in range(len(board)):
+        #         if board[i][j] == '.':
+        #             continue
+        #         if (board[i][j] in rowscount[i] or
+        #             board[i][j] in columncount[j] or
+        #             board[i][j] in submatrixcount[i//3,j//3] ):
+        #             return False
+        #         rowscount[i].add(board[i][j])
+        #         columncount[j].add(board[i][j])
+        #         submatrixcount[i//3,j//3].add(board[i][j])
+        # return True
+# way2:
+        rowscount = defaultdict(set)
+        columncount = defaultdict(set)
+
+
+        for i in range(len(board)):
+            for j in range(len(board)):
+                if board[i][j] == '.':
                     continue
-                if (board[row][column] in rows[row] or
-                   board[row][column] in columns[column] or
-                   board[row][column] in submatrix[row//3,column//3]):
-                   return False
-                columns[column].add(board[row][column])
-                rows[row].add(board[row][column])
-                submatrix[row//3, column//3].add(board[row][column])
+                if (board[i][j] in rowscount[i] or
+                    board[i][j] in columncount[j] ):
+                    return False
+                rowscount[i].add(board[i][j])
+                columncount[j].add(board[i][j])
+        submatrices=[[0,0],[0,3],[0,6],[3,0],[3,3],[3,6],[6,0],[6,3],[6,6]]
+        for x,y in submatrices:  
+            submatrix = set()              
+            for i in range(x,x+3):
+                for j in range(y,y+3):
+                    if board[i][j] == '.':
+                        continue
+                    if (board[i][j] in submatrix):
+                        return False
+                    submatrix.add(board[i][j])
         return True
