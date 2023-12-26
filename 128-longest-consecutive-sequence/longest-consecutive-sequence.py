3c83ad4680a51e94d314aca1a4b672fcd3236957
Raja Ravindra Kathi <rajaravindra919@gmail.com>
2023-12-26 03:18:30 -0800

    Time: 331 ms (99.65%) | Memory: 32.5 MB (20.19%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -5,14 +5,14 @@ class Solution:
 # -> image a number line of these each elements, for consecuitive there is no left neighbour.
 # -> if there is no left neighbour, we have to initiate length counting and then count till the next number exists in loop.
 # -> finally see if longest<length and update; at end of loop return 
-        numSet = set(nums)
         longest = 0
+        numSet  = set(nums)
         for num in numSet:
-            length = 1
             if num-1 not in numSet:
+                length = 1
                 while num+length in numSet:
                     length += 1
-            longest = max(length, longest)
+                longest = max(length, longest)
         return longest
 
 # solution:
@@ -20,13 +20,11 @@ class Solution:
 # sort numbers and then see if difference is 1 (means consecutive..); then keep counting till length increases..
 # here we convert sortednums to set because; `in` operation for set is O(1)
 # solution comes of O(nlogn) and time exceeds
-        # sortednums = set(sorted(nums))
-        # longest = 0
-        # for num in sortednums:
-        #     length = 1
-        #     while num + length in sortednums:
-        #         length += 1
-        #     longest = max(length, longest)
-        # return longest
-            
-                
+        nums.sort()
+        longest = 0
+        for index, num in enumerate(nums):
+            length = 1
+            while num+length in nums:
+                length += 1
+            longest = max(length, longest)
+        return longest
