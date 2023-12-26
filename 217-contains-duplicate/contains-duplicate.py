1e042ff4fbf494de5ce027f689f7719b46cde453
Raja Ravindra Kathi <rajaravindra919@gmail.com>
2023-12-25 16:03:24 -0800

    Time: 424 ms (97.61%) | Memory: 32.9 MB (24.28%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -3,17 +3,11 @@ class Solution:
 # solution:
 # -> for each element we visit, let us store it in a hashset (why not list? 'in' operation in list is O(n); in set is O(1))
 # -> for every element we see check in the hashset, if exists return True
-#         seenHashSet = set()
-#         for num in nums:
-#             if num in seenHashSet:
-#                 return True
-#             seenHashSet.add(num)
-#         return False
+        seenHashSet = set()
+        for num in nums:
+            if num in seenHashSet:
+                return True
+            seenHashSet.add(num)
+        return False
 # by sorting:
 # -> sort all the elements and then compare each other, none should be same
-            sortednums = sorted(nums)
-            print(sortednums)
-            for i in range(len(sortednums)-1):
-                if sortednums[i] == sortednums[i+1]:
-                    return True
-            return False
