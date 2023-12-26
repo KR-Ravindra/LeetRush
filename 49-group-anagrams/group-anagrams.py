b1ef3e4660d953eb9e9c3e793aa00258d6945014
Raja Ravindra Kathi <rajaravindra919@gmail.com>
2023-12-25 16:27:39 -0800

    Time: 88 ms (92.04%) | Memory: 23.3 MB (8.08%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -5,21 +5,20 @@ class Solution:
 # -> every string can only have characters from a-z (ascii 63-89 whatever); so for each str we will calculate the array[ascii 26]; this way if two strings are anagram their array[ascii 26] would be same. 
 # -> then for each key we will have a list of str as values in our new hashmap
 # -> return the values as list
-        # resultHashMap = defaultdict(list)
-        # for str in strs:
-        #     count = [0]*26
-        #     for char in str:
-        #         count[ord(char)-ord('a')] += 1
-        #     resultHashMap[(tuple(count))].append(str)
-        # return resultHashMap.values()
-
+        resultantHashMap = defaultdict(list)
+        for str in strs:
+            count = [0] * 26
+            for char in str:
+                count[ord(char)-ord('a')] += 1
+            resultantHashMap[tuple(count)].append(str)
+        return resultantHashMap.values()
 # -> sorting way:
 # -> if a string is anagram of other then their resultant sort should be same; for each str we will sort them
 # -> sorted str is key and value is list of str that match this sorted pattern ( aet: [eat, tea])
 # -> we return the values in list format for desired output
-        resultantHashMap = defaultdict(list)
-        for str in strs:
-            resultantHashMap[tuple(sorted(str))].append(str)
-        return resultantHashMap.values()
+        # resultantHashMap = defaultdict(list)
+        # for str in strs:
+        #     resultantHashMap[tuple(sorted(str))].append(str)
+        # return resultantHashMap.values()
             
 
