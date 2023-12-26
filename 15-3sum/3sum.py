c9005e75e211507fa1c823cad2d7fd5d76bade84
Raja Ravindra Kathi <rajaravindra919@gmail.com>
2023-12-26 03:36:50 -0800

    Time: 803 ms (89.19%) | Memory: 21.6 MB (8.84%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -5,21 +5,22 @@ class Solution:
         for index, num in enumerate(nums):
             if index>0 and nums[index-1] == num:
                 continue
-            
+
             left, right = index+1, len(nums)-1
-            while left < right:
-                if nums[left] + nums[right] + num == 0:
-                    triplets.append([num, nums[left], nums[right]])
-                if nums[left] + nums[right] + num > 0:
+            while left<right:
+                if num + nums[left] + nums[right] < 0:
+                    left += 1
+                    continue
+                if num + nums[left] + nums[right] > 0:
                     right -= 1
                     continue
-                if nums[left] + nums[right] + num < 0:
-                    left += 1
-                    continue
+                if num + nums[left] + nums[right] == 0 and left<right:
+                    triplets.append([num, nums[left], nums[right]])
+                
                 left += 1
                 right -= 1
-                while nums[left] == nums[left-1] and left < right:
+                while nums[left] == nums[left-1] and left<right:
                     left += 1
-                while nums[right] == nums[right+1] and left < right:
+                while nums[right] == nums[right+1] and left<right:
                     right -= 1
         return triplets
