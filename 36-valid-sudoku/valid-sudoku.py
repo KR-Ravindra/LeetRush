42f16e23a870b2c578997ac3e27eff1479c62ce2
Raja Ravindra Kathi <rajaravindra919@gmail.com>
2023-12-26 03:06:32 -0800

    Time: 93 ms (90.24%) | Memory: 17.2 MB (7.75%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -4,22 +4,25 @@ class Solution:
 # -> so for each row, column and submatrix we need to check if the set has any duplicate numbers. Let us ignore the `.` in input and then include all the numbers in each row and column.
 # -> how do we consider submatrices? - two ways, one predefine them (only works for this). second is every location(x,y) in 9*9 submatrix on integer division with 3, will always belong to one of 3*3 sub matrix - eg (4,4) in 9,9 => (1,1)
 # way1:
-        # rowscount = defaultdict(set)
-        # columncount = defaultdict(set)
-        # submatrixcount = defaultdict(set)
+        row = defaultdict(set)
+        column = defaultdict(set)
+        submatrix = defaultdict(set)
 
-        # for i in range(len(board)):
-        #     for j in range(len(board)):
-        #         if board[i][j] == '.':
-        #             continue
-        #         if (board[i][j] in rowscount[i] or
-        #             board[i][j] in columncount[j] or
-        #             board[i][j] in submatrixcount[i//3,j//3] ):
-        #             return False
-        #         rowscount[i].add(board[i][j])
-        #         columncount[j].add(board[i][j])
-        #         submatrixcount[i//3,j//3].add(board[i][j])
-        # return True
+        for index in range(len(board)):
+            for secondindex in range(len(board)):
+                if board[index][secondindex] == '.':
+                    continue
+                if (
+                    board[index][secondindex] in row[index] or
+                    board[index][secondindex] in column[secondindex] or
+                    board[index][secondindex] in submatrix[index//3,secondindex//3]
+                ):
+                    return False
+                row[index].add(board[index][secondindex])
+                column[secondindex].add(board[index][secondindex])
+                submatrix[index//3,secondindex//3].add(board[index][secondindex])
+        return True
+
 # way2: manually building submatrices
         rowscount = defaultdict(set)
         columncount = defaultdict(set)
