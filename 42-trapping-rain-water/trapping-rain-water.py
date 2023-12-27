3776bf166a87741b1acd6eab91c815b9a23953d5
Raja Ravindra Kathi <rajaravindra919@gmail.com>
2023-12-27 02:56:26 -0800

    Time: 105 ms (85.90%) | Memory: 19.2 MB (8.63%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,21 +1,19 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+        # algorithm = watertrapped is given by min(maxlef,maxright) - height[i]
         maxLeft = height[0]
         maxRight = height[len(height)-1]
         water_collected = 0
         left, right = 0, len(height)-1
         while left<right:
-            print(f"{maxLeft=} {maxRight=}")
             if maxLeft < maxRight:
                 left += 1
                 if min(maxLeft, maxRight) - height[left] > 0:
-                    print(f"Adding { min(maxLeft, maxRight) - height[left]} {height[left]=}")
                     water_collected +=  min(maxLeft, maxRight) - height[left]
                 maxLeft = max(maxLeft, height[left])
             else:
                 right -= 1
                 if  min(maxLeft, maxRight) - height[right] > 0:
-                    print(f"Adding { min(maxLeft, maxRight) - height[right]} {height[right]=}")
                     water_collected +=  min(maxLeft, maxRight) - height[right]
                 maxRight = max(maxRight, height[right])
         return water_collected
