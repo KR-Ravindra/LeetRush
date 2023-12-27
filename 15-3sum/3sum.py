93f52969c8dc6a5466cc5e522d1711f29b00555c
Raja Ravindra Kathi <rajaravindra919@gmail.com>
2023-12-27 13:54:27 -0800

    Time: 1073 ms (59.90%) | Memory: 21.5 MB (16.79%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -2,21 +2,19 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         triplets = []
-        for index, num in enumerate(nums):
-            if index>0 and nums[index-1] == num:
+        for i in range(len(nums)):
+            if i>0 and nums[i-1] == nums[i]:
                 continue
-
-            left, right = index+1, len(nums)-1
+            left, right = i+1, len(nums)-1
             while left<right:
-                if num + nums[left] + nums[right] < 0:
-                    left += 1
-                    continue
-                if num + nums[left] + nums[right] > 0:
+                if nums[left] + nums[right] + nums[i] == 0 and left<right:
+                    triplets.append([nums[left] , nums[right] , nums[i]])
+                if nums[left] + nums[right] + nums[i] > 0:
                     right -= 1
                     continue
-                if num + nums[left] + nums[right] == 0 and left<right:
-                    triplets.append([num, nums[left], nums[right]])
-                
+                if nums[left] + nums[right] + nums[i] < 0:
+                    left += 1
+                    continue
                 left += 1
                 right -= 1
                 while nums[left] == nums[left-1] and left<right:
@@ -24,3 +22,4 @@ class Solution:
                 while nums[right] == nums[right+1] and left<right:
                     right -= 1
         return triplets
+                
