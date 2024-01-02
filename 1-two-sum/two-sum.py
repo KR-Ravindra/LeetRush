842434c59197ff39bf2b3ce21cf211886f67164a
Raja Ravindra Kathi <rajaravindra919@gmail.com>
2024-01-01 16:09:23 -0800

    Time: 121 ms (42.44%) | Memory: 18.7 MB (9.09%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -4,9 +4,9 @@ class Solution:
 # -> for each number let us subtract it from target and see if the difference exists in list (but this `in` is a big problem O(n))
 # -> let us maintain HashMap and keep storing the numbers we see into it, and check if the diff is in the hashmap instead of whole list which is O(1)
 # -> This way we will immediately know the solution in O(n)
-        seenHashMap = defaultdict(int)
-        for index, num in enumerate(nums):
-            if target-num in seenHashMap:
-                return [index, seenHashMap[target-num]]
-            seenHashMap[num] = index
-        return seenHashMap
+        seen = defaultdict(int)
+        for index, each in enumerate(nums):
+            if target-each in seen:
+                return [index, seen[target-each]]
+            seen[each] = index
+        return False
