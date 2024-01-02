ea2de714dd3d089bd4a8582b7a509ef2b83d504b
Raja Ravindra Kathi <rajaravindra919@gmail.com>
2024-01-01 16:23:19 -0800

    Time: 104 ms (34.04%) | Memory: 23.1 MB (6.88%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -4,16 +4,19 @@ class Solution:
 # -> First we will calculate the count of each character/number
 # -> then we plot it on frequency array, here frequency array could only be as big as len(nums)
 # -> we reverse iterate through our frequency array ([6: {1,2}, 5: {2,3}....]) and fill result list with the variable we have in set till k is len(result)
-        count = defaultdict(int)
+        countHashMap = defaultdict(int)
+
+        for eachnum in nums:
+            countHashMap[eachnum] += 1
+        
+        frequencyList = [[] for i in range(len(nums))]
+        print(frequencyList)
+        for num, value in countHashMap.items():
+            frequencyList[value-1].append(num)
+        
         result = []
-        for num in nums:
-            count[num] += 1
-        frequency = [[] for i in range(len(nums)+1)]
-        for each, values in count.items():
-            frequency[values].append(each)
-        for each in frequency[::-1]:
-            for eachlistitem in each:
-                result.append(eachlistitem)
+        for each in frequencyList[::-1]:
+            for eachnum in each:
+                result.append(eachnum)
                 if len(result) == k:
                     return result
-        return False
