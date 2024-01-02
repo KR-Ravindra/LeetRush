5ea81fd26f90ebddba5a9ee0588aa3b4ebe3e2dd
Raja Ravindra Kathi <rajaravindra919@gmail.com>
2024-01-01 16:38:53 -0800

    Time: 385 ms (5.02%) | Memory: 24.4 MB (42.28%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -5,22 +5,22 @@ class Solution:
 # -> we compute prefix and postfix prodcut arrays; prefix array has at every index product of numbers from main nums array till index (not including index); similarly postfix has product till before that number from end of nums.
 # -> for prefix we traverse nums from 0 to end; postfix we do it end to 0
 # -> at last, for result array we multiply prefix[i]*postfix[i]
-        preproduct = [1] * len(nums)
-        postproduct = [1] * len(nums)
-        result = [1] * len(nums)
-        print(f"{preproduct=}{postproduct=}{result=}")
-        for index in range(len(nums)):
-            if index == 0:
-                continue
-            preproduct[index] *= nums[index-1] * preproduct[index-1]
-        print(f"{nums=}{preproduct=}")
-        for index in range(len(nums)-1,-1,-1):
-            if index == len(nums)-1:
-                continue
-            postproduct[index] = nums[index+1] * postproduct[index+1]
-        for index in range(len(nums)):
-            result[index] *= preproduct[index] * postproduct[index]
-        return result
+        # preproduct = [1] * len(nums)
+        # postproduct = [1] * len(nums)
+        # result = [1] * len(nums)
+        # print(f"{preproduct=}{postproduct=}{result=}")
+        # for index in range(len(nums)):
+        #     if index == 0:
+        #         continue
+        #     preproduct[index] *= nums[index-1] * preproduct[index-1]
+        # print(f"{nums=}{preproduct=}")
+        # for index in range(len(nums)-1,-1,-1):
+        #     if index == len(nums)-1:
+        #         continue
+        #     postproduct[index] = nums[index+1] * postproduct[index+1]
+        # for index in range(len(nums)):
+        #     result[index] *= preproduct[index] * postproduct[index]
+        # return result
 
 # with space complexity as O(1):
 # -> instead of building two post and prefix arrays let us use variables and keep track of product and directly manipulate final array
@@ -28,10 +28,11 @@ class Solution:
         prefix = 1
         postfix = 1
         result = [1]*len(nums)
-        for index in range(len(nums)):
-            result[index] = prefix
-            prefix *= nums[index]
-        for index in range(len(nums)-1,-1,-1):
+        for index in range(1,len(nums),1):
+            prefix *= nums[index-1]
+            result[index] *= prefix
+
+        for index in range(len(nums)-2, -1 , -1):
+            postfix *= nums[index+1]
             result[index] *= postfix
-            postfix *= nums[index]
         return result
