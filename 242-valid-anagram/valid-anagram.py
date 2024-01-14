637c9c7bd948f256d8424595f08a9bfa127b4784
Raja Ravindra Kathi <rajaravindra919@gmail.com>
2024-01-13 19:42:51 -0800

    Time: 61 ms (25.11%) | Memory: 17.7 MB (23.26%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -7,14 +7,15 @@ class Solution:
             return False
 # by algorithm:
 # -> count the frequency of characters into a hashmap and then compare both; if their hashmap is same then they are anagram
-        countS, countT = defaultdict(int), defaultdict(int)
+        countS , countT = defaultdict(int), defaultdict(int)
         for i in range(len(s)):
             countS[s[i]] += 1
             countT[t[i]] += 1
-        for each in countS:
-            if not countS[each] == countT[each]:
+        for keys, values in countS.items():
+            if values != countT[keys]:
                 return False
         return True
+            
 # by sorting:
 # -> if both of their sorted inputs are same then they both are anagrams
         # return sorted(s) == sorted(t)
