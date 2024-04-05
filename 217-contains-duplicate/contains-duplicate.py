aa2e2796925771f3e9a806b93064e7e8e03d2e07
Raja Ravindra Kathi <rajaravindra919@gmail.com>
2024-04-05 11:45:40 -0700

    Time: 430 ms (29.97%) | Memory: 31.9 MB (65.61%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -3,13 +3,13 @@ class Solution:
 # solution:
 # -> for each element we visit, let us store it in a hashset (why not list? 'in' operation in list is O(n); in set is O(1))
 # -> for every element we see check in the hashset, if exists return True
-        seen = set()
-        for each in nums:
-            if each in seen:
-                return True
-            else:
-                seen.add(each)
-        return False
+        # seen = set()
+        # for each in nums:
+        #     if each in seen:
+        #         return True
+        #     else:
+        #         seen.add(each)
+        # return False
 # by sorting:
 # -> sort all the elements and then compare each other, if any same then we have duplicate
         # nums.sort()
@@ -18,3 +18,5 @@ class Solution:
         #         if nums[index+1] == num:
         #             return True
         # return False
+# by comparing lenght of array and its set:
+            return not len(nums) == len(set(nums))
