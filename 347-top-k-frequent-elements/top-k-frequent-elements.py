23063260e9ad2510744f7fe71e1fca0997ecf3f9
Raja Ravindra Kathi <rajaravindra919@gmail.com>
2024-10-14 23:09:33 -0700

    Time: 80 ms (92.16%) | Memory: 22.4 MB (8.39%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -5,18 +5,18 @@ class Solution:
 # -> then we plot it on frequency array, here frequency array could only be as big as len(nums)
 # -> we reverse iterate through our frequency array ([6: {1,2}, 5: {2,3}....]) and fill result list with the variable we have in set till k is len(result)
         countHashMap = defaultdict(int)
-        for each in nums:
-            countHashMap[each] += 1
+
+        for num in nums:
+            countHashMap[num] += 1
         
-        frequency = [[] for i in range(len(nums)+1)]
-        for number, count in countHashMap.items():
-            frequency[count].append(number)
+        frequency_array = [[] for i in range(len(nums)+1)]
+        print(frequency_array)
+        for keys, values in countHashMap.items():
+            frequency_array[values].append(keys)
         
         result = []
-        for each in frequency[::-1]:
-            for eachElement in each:
-                result.append(eachElement)
-            if len(result) == k:
-                return result
-        
-        return False
+        for eachList in frequency_array[::-1]:
+            for each in eachList:
+                result.append(each)
+                if len(result) == k:
+                    return result
