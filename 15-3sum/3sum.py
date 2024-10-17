ce7ed6ee4b26c2e48abc34cf138fea6d713f0d37
Raja Ravindra Kathi <rajaravindra919@gmail.com>
2024-10-16 21:50:19 -0700

    Time: 1169 ms (18.70%) | Memory: 20.7 MB (42.18%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,25 +1,25 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        triplets = []
-        for i in range(len(nums)):
-            if i>0 and nums[i-1] == nums[i]:
+        sortedNums = sorted(nums)
+        result = []
+        for a in range(len(nums)):
+            if a>0 and sortedNums[a-1] == sortedNums[a]:
                 continue
-            left, right = i+1, len(nums)-1
-            while left<right:
-                if nums[left] + nums[right] + nums[i] == 0 and left<right:
-                    triplets.append([nums[left] , nums[right] , nums[i]])
-                if nums[left] + nums[right] + nums[i] > 0:
+            left, right = a+1, len(nums)-1
+            while left < right:
+                if sortedNums[a] + sortedNums[left] + sortedNums[right] == 0:
+                    result.append([sortedNums[a], sortedNums[left], sortedNums[right]])
+                    left += 1
                     right -= 1
-                    continue
-                if nums[left] + nums[right] + nums[i] < 0:
+                    while sortedNums[left] == sortedNums[left-1] and left<right:
+                        left += 1
+                    while sortedNums[right] == sortedNums[right+1] and left<right:
+                        right -= 1
+                if sortedNums[a] + sortedNums[left] + sortedNums[right] < 0:
                     left += 1
                     continue
-                left += 1
-                right -= 1
-                while nums[left] == nums[left-1] and left<right:
-                    left += 1
-                while nums[right] == nums[right+1] and left<right:
+                if sortedNums[a] + sortedNums[left] + sortedNums[right] > 0:
                     right -= 1
-        return triplets
+                    continue
+        return result
                 
