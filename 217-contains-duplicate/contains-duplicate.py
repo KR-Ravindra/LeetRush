583ea8cd40e90d26dde02f44c27e38b611b1d615
KR Ravindra <rajaravindra919@gmail.com>
2025-06-17 15:06:16 -0700

    Time: 9 ms (82.36%) | Memory: 31.5 MB (82.83%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,24 +1,9 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-# solution:
-# -> for each element we visit, let us store it in a hashset (why not list? 'in' operation in list is O(n); in set is O(1))
-# -> for every element we see check in the hashset, if exists return True
-        seen = set()
-        if len(nums) == 0:
-            return False
-        for num in nums:
-            if num in seen:
+        occurred = set()
+
+        for eachnum in nums:
+            if eachnum in occurred:
                 return True
-            else:
-                seen.add(num)
+            occurred.add(eachnum)
         return False
-# by sorting:
-# -> sort all the elements and then compare each other, if any same then we have duplicate
-        # nums.sort()
-        # for index, num in enumerate(nums):
-        #     if index+1 <=len(nums)-1:
-        #         if nums[index+1] == num:
-        #             return True
-        # return False
-# by comparing lenght of array and its set:
-            # return not len(nums) == len(set(nums))
