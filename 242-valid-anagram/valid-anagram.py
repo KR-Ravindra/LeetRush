e4191bdb239a683297c0e3460a1b06d40b94cf0a
KR Ravindra <rajaravindra919@gmail.com>
2025-06-17 15:24:46 -0700

    Time: 11 ms (72.78%) | Memory: 18.1 MB (32.26%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,24 +1,16 @@
 class Solution:
-
     def isAnagram(self, s: str, t: str) -> bool:
-# solution:
-# -> first check if both strings are same length if not then it is not anagaram
+
         if len(s) != len(t):
             return False
-# by algorithm:
-# -> count the frequency of characters into a hashmap and then compare both; if their hashmap is same then they are anagram
-        countS, countT = defaultdict(int), defaultdict(int)
-        for each in s:
-            countS[each] += 1
-        for each in t:
-            countT[each] += 1
-        for keys, values in countS.items():
-            if values == countT[keys]:
-                continue;
-            else:
-                return False
-        return True
+        
+        first = {}
+        second = {}
+
+        for eachS in s:
+            first[eachS] = first.get(eachS, 0 ) + 1
+        for eachT in t:
+            second[eachT] = second.get(eachT, 0) + 1
+        
+        return first == second
             
-# by sorting:
-# -> if both of their sorted inputs are same then they both are anagrams
-        # return sorted(s) == sorted(t)
